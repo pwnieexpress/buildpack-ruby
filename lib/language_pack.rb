8f11aaff53919ea47d93b4ca246895fd4bbef276
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2016-09-16 10:48:33 -0400

    Strip out rails 2 & 3 support

--- lib/language_pack.rb
+++ lib/language_pack.rb
@@ -13,7 +13,7 @@ module LanguagePack
     Instrument.instrument 'detect' do
       Dir.chdir(args.first)
 
-      pack = [ NoLockfile, Rails5, Rails42, Rails41, Rails4, Rails3, Rails2, Rack, Ruby ].detect do |klass|
+      pack = [ NoLockfile, Rails5, Rails42, Rails41, Rails4, Rack, Ruby ].detect do |klass|
         klass.use?
       end
 
@@ -43,8 +43,6 @@ require "language_pack/helpers/bundler_wrapper"
 
 require "language_pack/ruby"
 require "language_pack/rack"
-require "language_pack/rails2"
-require "language_pack/rails3"
 require "language_pack/disable_deploys"
 require "language_pack/rails4"
 require "language_pack/rails41"
